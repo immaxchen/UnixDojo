600a47af22f62ec4489a56e2ce7dddf4d24e3099
unknown <immaxchen@gmail.com>
2021-05-30 21:20:52 +0800

    update fork.pl

--- perl/fork.pl
+++ perl/fork.pl
@@ -6,46 +6,47 @@ use warnings;
 $|=1;
 
 sub dispatch {
-	my $children = shift;
-	my $pn = scalar @$children;
-	my $pi = 0;
-	while (my $line = <STDIN>) {
-		$pi = $pi++ % $pn;
-		print { $children->[$pi] } $line;
-	}
+    my $children = shift;
+    my $pn = scalar @$children;
+    my $pi = 0;
+    while (my $line = <STDIN>) {
+        $pi = $pi % $pn;
+        print { $children->[$pi] } $line;
+        $pi++;
+    }
 }
 
 sub dowork {
-	my $cnt = 0;
-	while (my $line = <STDIN>) {
-		$cnt++;
-	}
-	print "$cnt\n";
+    my $cnt = 0;
+    while (my $line = <STDIN>) {
+        $cnt++;
+    }
+    print "$cnt\n";
 }
 
 sub main {
-	my $pn = shift;
-	my @children = ();
-	my $pid = 1;
-	for my $pi (1 .. $pn) {
-		if ($pid) { # parent
-			$pid = open(my $child, "|-") // die "Can't fork: $!";
-			push @children, $child;
-		}
-	}
-	if ($pid) { # parent
-		dispatch \@children;
-		close $_ for @children;
-		wait for @children;
-	} else { # child
-		close $_ for @children;
-		dowork;
-		exit;
-	}
+    my $pn = shift;
+    my @children = ();
+    my $pid = 1;
+    for my $pi (1 .. $pn) {
+        if ($pid) { # parent
+            $pid = open(my $child, "|-") // die "Can't fork: $!";
+            push @children, $child;
+        }
+    }
+    if ($pid) { # parent
+        dispatch \@children;
+        close $_ for @children;
+        wait for @children;
+    } else { # child
+        close $_ for @children;
+        dowork;
+        exit;
+    }
 }
 
 unless (caller) {
-	($#ARGV == 0) or die "Error: need exactly 1 input argument (process count)\n";
-	$ARGV[0] > 0 or die "Error: incorrect number for process count\n";
-	main($ARGV[0]);
+    ($#ARGV == 0) or die "Error: need exactly 1 input argument (process count)\n";
+    $ARGV[0] > 0 or die "Error: incorrect number for process count\n";
+    main($ARGV[0]);
 }
