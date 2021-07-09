f6fdb96395661757fb46366ee5a84632b1cc1ce7
unknown <immaxchen@gmail.com>
2021-07-09 22:03:38 +0800

    update perl Build install method

--- note/install_perl_packages_locally.sh
+++ note/install_perl_packages_locally.sh
@@ -5,11 +5,20 @@ tar -xz -f somepackage.tar.gz
 
 cd somepackage
 
+# Makefile.PL
+
 perl -I/path/to/include Makefile.PL PREFIX=/path/to/install
 
 make
 
 make install
 
-# use lib '/path/to/install/share/perl5';
+# Build.PL
 
+perl -I/path/to/include Build.PL
+
+./Build
+
+./Build install --destdir /path/to/install
+
+# use lib '/path/to/install/share/perl5';
